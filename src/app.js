64f0cacb37a4c89c1e41d5d95e866c2fd0e294ff
Shuang Tian <shuangtian@nbp-166-19.nbp.ruw.rutgers.edu>
2017-08-14 15:38:00 -0400

    generate index.html using gulp  & refactor

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import angular from 'angular';
+import '../style/app.css';
+
+let app = () => {
+  return {
+    template: require('./app.html'),
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  }
+};
+
+class AppCtrl {
+  constructor($http, $scope){
+  // read the json file
+    $http.get("../data/rest.json").then(function(data) {
+    // console.log(data.data);
+    // console.log(typeof(data));
+    $scope.contents = data.data;
+    //debugger;
+   });
+  }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [])
+  .directive('app', app)
+  .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,25 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = function makeConfig() {
     var config = {};
 
-    config.entry = "./script.js";
+    config.entry = "./src/app.js";
     config.output = {
         path: __dirname,
         filename: "bundle.js"
     };
     config.module = {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.js$/, loader: 'babel', exclude: /node_modules/},
+            { test: /\.html$/, loader: "raw" }
         ]
     };
+    config.plugins = [new HtmlWebpackPlugin({
+			template: './src/index.html',
+			inject: 'body'
+			})
+		     ]
     return config;
 }();
 
